1d2ef665bef81a4b0dfc6a7ab4f2ec7f6fe8d93d
veeksha45 <veeksha45@gmail.com>
2025-07-24 10:34:53 +0530

    company review

---
+++ utils/db-connection.js
@@ -0,0 +1,10 @@
+
+
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("testDB", "root", "veeksha", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+module.exports = sequelize;
